de0765d6a42ad7ccfa9da9d4ae4a08ee6660aab5
omarlagargola <79851501+omarlagargola@users.noreply.github.com>
2024-09-09 22:18:53 +0200

    Upload project

---
+++ src/components/ListComponent.vue
@@ -0,0 +1,25 @@
+<template>
+  <ul>
+    <li v-for="(item, index) in dataItems" :key="index" @click="setToTop(index)">
+      {{ item }}
+    </li>
+  </ul>
+</template>
+
+<script setup>
+  import { ref } from 'vue'
+  const props = defineProps(['items']);
+  
+  const dataItems = ref(props.items);
+
+  const setToTop = (index) => {
+    const toTop = dataItems.value.splice(index, 1);
+    dataItems.value.unshift(...toTop);
+  }
+</script>
+
+<style>
+  li{
+    cursor: pointer;
+  }
+</style>
---
+++ src/components/RatingComponent.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="rating">
+    <span v-for="(index, value) in [0,1,2,3,4]" :key="index" :class="{'active': index <= rating}" @click="rate(index)">*</span>
+  </div>
+</template>
+
+<script setup>
+  import { ref } from 'vue'
+  
+  const rating = ref(-1);
+  
+  const rate = (index) => {
+    rating.value = index;
+  };
+</script>
+
+<style>
+  span{
+    cursor: pointer;
+  }
+</style>
